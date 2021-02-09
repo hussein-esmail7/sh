fbc36b59fa64cac0296d59298435ca04b4647c43
Hussein Esmail <hussein.esmail7@gmail.com>
2021-02-09 12:09:13 -0500

    Updated ip.sh

--- ip.sh
+++ ip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-local=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
+local=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | grep '192.168.')
 ext=$(curl -s ipinfo.io/ip)
 echo "External: $ext
 Local:    $local"
