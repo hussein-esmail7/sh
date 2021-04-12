1919aae00829b22e72763dcc4be69a830af06851
Hussein Esmail <hussein.esmail7@gmail.com>
2021-04-12 13:43:30 -0400

    deleted:    groff-template.sh 	modified:   update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,16 @@
 # I don't have to worry about 
 # updating specific apps
 
-brew -q upgrade
-pip3 -q --no-input install --upgrade pip
-python3 -m pip install --upgrade pip
-/Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app/Contents/MacOS/msupdate --install --apps MSWD2019 XCEL2019 PPT32019 > /dev/null
+os=$(uname -a | awk '{print $(1);}') # This command works on macOS and Linux
+
+if [[ os ~= "Linux" ]]; then # Linux update lines
+    newsboat -x reload  # Update newsboat feeds
+    sudo pacman -Syyu   # Update pacman packages
+    yay -Syyu --devel   # Update AUR packages + git dependencies
+    pip install --upgrade pip # Upgrade pip
+elif [[ os ~= "Darwin" ]]; then # macOS update lines
+    brew -q upgrade     # Update Homebrew packages
+    pip3 -q --no-input install --upgrade pip # Upgrade pip
+    python3 -m pip install --upgrade pip
+    /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app/Contents/MacOS/msupdate --install --apps MSWD2019 XCEL2019 PPT32019 > /dev/null
+fi
