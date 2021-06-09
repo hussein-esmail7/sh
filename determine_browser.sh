fc5d2d0d4c67baf72ae97d2786f307ded59bc2c4
Hussein Esmail <hussein.esmail7@gmail.com>
2021-06-09 16:13:58 -0400

    modified:   determine_browser.sh

--- determine_browser.sh
+++ determine_browser.sh
@@ -8,9 +8,8 @@
 
 NOTIFY_ON_OPEN=0
 
-PROGRAM_IMAGE="gwenview"
+PROGRAM_IMAGE="mpv --no-terminal --keep-open=yes --osd-level=1 -osd-font-size=18 --geometry=100%:50% --autofit=50% --pause"
 PROGRAM_VIDEO="mpv --no-terminal --keep-open=yes --osd-level=1 -osd-font-size=18 --geometry=100%:50% --autofit=50%"
-PROGRAM_OTHER="brave"
 # TODO: mp3 for podcasts
 
 NO_OUTPUT="&>/dev/null &"
@@ -21,16 +20,25 @@ YT_Check_2="youtu.be/"
 Opened_app="null"
 
 if [[ "$URL" == *".png"* || "$URL" == *".jpg"* || "$URL" == *".jpeg"* || "$URL" == *".gif"* || "$URL" == *".tiff"* || "$URL" == *".bmp"* ]]; then
+    # If it is an image
+    if [[ "$URL" == *"preview.redd.it/"* ]]; then   # Support for https://preview.redd.it/... URLs
+        URL=${URL/preview/i}                        # Replace "preview" with "i" since it is an image
+        URL=$(echo "$URL" | cut -f1 -d"?")          # Get rid of the display arguments and just pass the image link
+    fi
     eval "$PROGRAM_IMAGE $URL $NO_OUTPUT" 
     Opened_app=$(echo "$PROGRAM_IMAGE" | head -n1 | cut -d " " -f1)
+elif [[ "$URL" == *"reddit.com/gallery/"* ]]; then  # Reddit Gallery images
+    
+
+
 elif [[ "$URL" == *"$YT_Check_1"* ||  "$URL" == *"videos.lukesmith.xyz"* || "$URL" == *"peertube"* || "$URL" == *"v.redd.it/"* ]]; then
     # if it is a YouTube video, on a PeerTube instance, or a Reddit video
     # Used to be: "$URL" == *"$YT_Check_2"* || $(curl -s $URL | grep -ic "PeerTube") -ge 1 but it took too long to process each time
     eval "$PROGRAM_VIDEO $URL & 2>/dev/null"
     Opened_app=$(echo "$PROGRAM_VIDEO" | head -n1 | cut -d " " -f1)
 else
-    eval "$PROGRAM_OTHER $URL &> /dev/null 2>&1 &"
-    Opened_app=$(echo "$PROGRAM_OTHER" | head -n1 | cut -d " " -f1)
+    eval "$BROWSER $URL &> /dev/null 2>&1 &"
+    Opened_app=$(echo "$BROWSER" | head -n1 | cut -d " " -f1)
 fi
 
 if [ $NOTIFY_ON_OPEN -eq 1 ]; then
