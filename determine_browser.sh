3b00b4f5edd38af73efb9edb0cd19cbe95ecbcdc
Hussein Esmail <hussein.esmail7@gmail.com>
2021-06-20 14:05:14 -0400

    modified:   determine_browser.sh 	new file:   reddit_gallery.py

--- determine_browser.sh
+++ determine_browser.sh
@@ -6,15 +6,20 @@
 # Updated: 2021 04 13
 # Description: This program determines what application to open depending on the type of URL (specifically made for newsboat on Linux)
 
-NOTIFY_ON_OPEN=0
+NOTIFY_ON_OPEN=0    # 1 for notification
+VID_SCREEN=2        # Open on 3rd monitor
 
-PROGRAM_IMAGE="mpv --no-terminal --keep-open=yes --osd-level=1 -osd-font-size=18 --geometry=100%:50% --autofit=50% --pause"
-PROGRAM_VIDEO="mpv --no-terminal --keep-open=yes --osd-level=1 -osd-font-size=18 --geometry=100%:50% --autofit=50%"
-# TODO: mp3 for podcasts
+PROG_IMG="feh -q. -g 1000x1000"
+PROG_VID="mpv --screen=$VID_SCREEN --no-terminal --keep-open=yes --osd-level=1 -osd-font-size=18 --geometry=0:0 --autofit=100%"
+PROG_AUDIO="mpv --screen=$VID_SCREEN --no-terminal --keep-open=yes --osd-level=1 -osd-font-size=18 --geometry=85%:80% --autofit=60% --force-window=yes"
 
-NO_OUTPUT="&>/dev/null &"
+NO_OUTPUT="&>/dev/null"
+PATH_REDDIT_GALLERY_PY="/home/hussein/git/sh/reddit_gallery.py"
 
-URL="$1"
+# URL without the arguments after '?'
+# URL="$1"
+URL=$(echo "$1" | cut -f1 -d"?")
+
 YT_Check_1="youtube.com/watch?v="
 YT_Check_2="youtu.be/"
 Opened_app="null"
@@ -25,17 +30,21 @@ if [[ "$URL" == *".png"* || "$URL" == *".jpg"* || "$URL" == *".jpeg"* || "$URL" 
         URL=${URL/preview/i}                        # Replace "preview" with "i" since it is an image
         URL=$(echo "$URL" | cut -f1 -d"?")          # Get rid of the display arguments and just pass the image link
     fi
-    eval "$PROGRAM_IMAGE $URL $NO_OUTPUT" 
-    Opened_app=$(echo "$PROGRAM_IMAGE" | head -n1 | cut -d " " -f1)
+    eval "$PROG_IMG $URL $NO_OUTPUT &" 
+    Opened_app=$(echo "$PROG_IMG" | head -n1 | cut -d " " -f1)
 elif [[ "$URL" == *"reddit.com/gallery/"* ]]; then  # Reddit Gallery images
-    
-
-
-elif [[ "$URL" == *"$YT_Check_1"* ||  "$URL" == *"videos.lukesmith.xyz"* || "$URL" == *"peertube"* || "$URL" == *"v.redd.it/"* ]]; then
+    # Get the individual image URLs and open them all in one image viewer window
+    eval "$PROG_IMG $(python3 $PATH_REDDIT_GALLERY_PY $URL) $NO_OUTPUT &"
+    Opened_app=$(echo "$PROG_IMG" | head -n1 | cut -d " " -f1)
+elif [[ "$URL" == *"$YT_Check_1"* ||  "$URL" == *"videos."* || "$URL" == *"peertube"* || "$URL" == *"v.redd.it/"* ]]; then
     # if it is a YouTube video, on a PeerTube instance, or a Reddit video
     # Used to be: "$URL" == *"$YT_Check_2"* || $(curl -s $URL | grep -ic "PeerTube") -ge 1 but it took too long to process each time
-    eval "$PROGRAM_VIDEO $URL & 2>/dev/null"
-    Opened_app=$(echo "$PROGRAM_VIDEO" | head -n1 | cut -d " " -f1)
+    eval "$PROG_VID $URL & 2>/dev/null"
+    Opened_app=$(echo "$PROG_VID" | head -n1 | cut -d " " -f1)
+elif [[ "$URL" == *".mp3"* ||  "$URL" == *".flac"* || "$URL" == *".m4a"* || "$URL" == *".wav"* || "$URL" == *".wma"* || "$URL" == *".aac"* ]]; then
+    # Audio, mainly for podcasts
+    eval "$PROG_AUDIO $URL & 2>/dev/null"
+    Opened_app=$(echo "$PROG_AUDIO" | head -n1 | cut -d " " -f1)
 else
     eval "$BROWSER $URL &> /dev/null 2>&1 &"
     Opened_app=$(echo "$BROWSER" | head -n1 | cut -d " " -f1)
---
+++ reddit_gallery.py
@@ -0,0 +1,54 @@
+'''
+reddit_gallery.py
+Hussein Esmail
+Created: 2021 06 09
+Updated: 2021 06 20
+Description: This program prints the individual Reddit image URLs from a Reddit Gallery link.
+'''
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import sys          # To exit the program and get arguments
+import platform     # To get the OS type
+import os           # To check file paths
+
+bravedriver_path_macos = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+chromedriver_path_macos = "/Users/hussein/Downloads/chromedriver"
+chromedriver_path_linux = "/home/hussein/Documents/Coding/Random/chromedriver"
+
+def main():
+    options = Options()  
+    options.add_argument("--headless")  # Run in background
+    os_type = platform.system()
+    if os_type == "Darwin":             # macOS
+        driver = webdriver.Chrome(chromedriver_path_macos, options=options)
+    elif os_type == "Linux": 
+        driver = webdriver.Chrome(chromedriver_path_linux, options=options)
+    else:                               # Windows
+        chromedriver_path_windows = input("Path of chromedriver for Windows: ")
+        if os.path.exists(chromedriver_path_windows):
+            driver = webdriver.Chrome(chromedriver_path_windows, options=options)
+        else:
+            print("Path not valid.")
+            sys.exit()
+
+    driver.get(sys.argv[-1])            # Open the target URL
+
+    pic_list = driver.find_element_by_xpath("//ul").find_elements_by_xpath(".//a")
+    pic_urls = []
+    for pic in pic_list:
+        pic_url = pic.get_attribute("href").split('?')[0]
+        if pic_url.endswith('.jpg'):
+            pic_url = pic_url.replace('preview.', 'i.')
+        else:                           # Unknown exceptions
+            print("Video or non-JPG, not done.")
+            print(pic_url)
+        pic_urls.append(pic_url)        # Add formatted URL to aray
+    print(" ".join(pic_urls))           # Print all URLs in one line
+    
+    driver.close()                      # Close the browser
+    options.extensions.clear()          # Clear the options that were set
+    sys.exit()                          # Exit the program
+
+if __name__ == "__main__":
+    main()
