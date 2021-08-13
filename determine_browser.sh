4de96f3cd8a1c19c1bd4f0f9608420fe074ac7ab
Hussein Esmail <hussein.esmail7@gmail.com>
2021-08-12 23:44:20 -0400

    modified:   determine_browser.sh

--- determine_browser.sh
+++ determine_browser.sh
@@ -28,54 +28,80 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     PROG_IMG="open"   # Was having issues on macos, so will use the default app
 fi
 
-HTML_CODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "$URL")
-if [[ $(echo "$HTML_CODE" | head -c 1) == "2" ]]; then
-    if [[ "$URL" == *".png"* || "$URL" == *".jpg"* || "$URL" == *".jpeg"* || "$URL" == *".gif"* || "$URL" == *".tiff"* || "$URL" == *".bmp"* ]]; then
-        # If it is an image
-        if [[ "$URL" == *"preview.redd.it/"* ]]; then   # Support for https://preview.redd.it/... URLs
-            URL=${URL/preview/i}                        # Replace "preview" with "i" since it is an image
-            URL=$(echo "$URL" | cut -f1 -d"?")          # Get rid of the display arguments and just pass the image link
-        fi
+if [[ "$URL" == *".png"* || "$URL" == *".jpg"* || "$URL" == *".jpeg"* || "$URL" == *".gif"* || "$URL" == *".tiff"* || "$URL" == *".bmp"* ]]; then
+    # If it is an image
+    HTML_CODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "$URL")
+    if [[ "$URL" == *"preview.redd.it/"* ]]; then   # Support for https://preview.redd.it/... URLs
+        URL=${URL/preview/i}                        # Replace "preview" with "i" since it is an image
+        URL=$(echo "$URL" | cut -f1 -d"?")          # Get rid of the display arguments and just pass the image link
+    fi
+    if [[ $(echo "$HTML_CODE" | head -c 1) == "2" ]]; then
         eval "$PROG_IMG $URL $NO_OUTPUT &" 
         Opened_app=$(echo "$PROG_IMG" | head -n1 | cut -d " " -f1)
-    elif [[ "$URL" == *"reddit.com/gallery/"* ]]; then  # Reddit Gallery images
-        # Get the individual image URLs and open them all in one image viewer window
+    else    # Non-200 HTML code
+        notify-send "Error $HTML_CODE" "Cannot open $URL"
+    fi
+elif [[ "$URL" == *"reddit.com/gallery/"* ]]; then  # Reddit Gallery images
+    # Get the individual image URLs and open them all in one image viewer window
+    HTML_CODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "$URL")
+    if [[ $(echo "$HTML_CODE" | head -c 1) == "2" ]]; then
         eval "$PROG_IMG $(python3 $PATH_REDDIT_GALLERY_PY $URL) $NO_OUTPUT &"
         Opened_app=$(echo "$PROG_IMG" | head -n1 | cut -d " " -f1)
-    elif [[ "$URL" == *"imgur.com/gallery/"* ]]; then  # Imgur Gallery images
-        # Get the individual image URLs and open them all in one image viewer window
-        
-        # TODO: FEH currently does not open .webp images, which Imgur returns. Find some way around this later.
-        # notify-send "Imgur link" "$URL"
-        # eval "$PROG_IMG $(python3 $PATH_IMGUR_GALLERY_PY $URL) $NO_OUTPUT &"
-        # Opened_app=$(echo "$PROG_IMG" | head -n1 | cut -d " " -f1)
+    else    # Non-200 HTML code
+        notify-send "Error $HTML_CODE" "Cannot open $URL"
+    fi
+elif [[ "$URL" == *"imgur.com/gallery/"* ]]; then  # Imgur Gallery images
+    # Get the individual image URLs and open them all in one image viewer window
+    
+    # TODO: FEH currently does not open .webp images, which Imgur returns. Find some way around this later.
+    # notify-send "Imgur link" "$URL"
+    # eval "$PROG_IMG $(python3 $PATH_IMGUR_GALLERY_PY $URL) $NO_OUTPUT &"
+    # Opened_app=$(echo "$PROG_IMG" | head -n1 | cut -d " " -f1)
 
-        # These lines are here until the above lines are fixed.
-        # eval "$BROWSER $URL &> /dev/null 2>&1 &"
-        # Opened_app=$(echo "$BROWSER" | head -n1 | cut -d " " -f1)
+    # These lines are here until the above lines are fixed.
+    # eval "$BROWSER $URL &> /dev/null 2>&1 &"
+    # Opened_app=$(echo "$BROWSER" | head -n1 | cut -d " " -f1)
 
+    HTML_CODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "$URL")
+    if [[ $(echo "$HTML_CODE" | head -c 1) == "2" ]]; then
         eval "$PROG_IMG $(python3 $PATH_IMGUR_GALLERY_PY $URL) $NO_OUTPUT &"
         Opened_app=$(echo "$PROG_IMG" | head -n1 | cut -d " " -f1)
-    elif [[ "$URL" == *"bibliogram.art/p/"* || "$URL" == *"bibliogram.snopyta.org/p/"* ]]; then  # Bibliogram Gallery images
-        # Get the individual image URLs and open them all in one image viewer window
+    else    # Non-200 HTML code
+        notify-send "Error $HTML_CODE" "Cannot open $URL"
+    fi
+elif [[ "$URL" == *"bibliogram.art/p/"* || "$URL" == *"bibliogram.snopyta.org/p/"* ]]; then  # Bibliogram Gallery images
+    # Get the individual image URLs and open them all in one image viewer window
+    HTML_CODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "$URL")
+    if [[ $(echo "$HTML_CODE" | head -c 1) == "2" ]]; then
         eval "$PROG_IMG $(python3 $PATH_BIBLIOGRAM_GALLERY_PY $URL) $NO_OUTPUT &"
         Opened_app=$(echo "$PROG_IMG" | head -n1 | cut -d " " -f1)
-    elif [[ "$URL" == *"youtube.com/watch?v="* || "$URL" == *"youtu.be/"* ||  "$URL" == *"videos."* || "$URL" == *"peertube"* || "$URL" == *"v.redd.it/"* || "$URL" == "https://bibliogram.art/videoproxy/"*  || "$URL" == "https://tinyurl.com/"* ]]; then
-        # if it is a YouTube video, on a PeerTube instance, or a Reddit video
-        # Used to be: "$URL" == *"youtu.be/"* || $(curl -s $URL | grep -ic "PeerTube") -ge 1 but it took too long to process each time
+    else    # Non-200 HTML code
+        notify-send "Error $HTML_CODE" "Cannot open $URL"
+    fi
+elif [[ "$URL" == *"youtube.com/watch?v="* || "$URL" == *"youtu.be/"* ||  "$URL" == *"videos."* || "$URL" == *"peertube"* || "$URL" == *"v.redd.it/"* || "$URL" == "https://bibliogram.art/videoproxy/"*  || "$URL" == "https://tinyurl.com/"* ]]; then
+    # if it is a YouTube video, on a PeerTube instance, or a Reddit video
+    # Used to be: "$URL" == *"youtu.be/"* || $(curl -s $URL | grep -ic "PeerTube") -ge 1 but it took too long to process each time
+    HTML_CODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "$URL")
+    if [[ $(echo "$HTML_CODE" | head -c 1) == "2" ]]; then
         eval "$PROG_VID $URL & 2>/dev/null"
         Opened_app=$(echo "$PROG_VID" | head -n1 | cut -d " " -f1)
-    elif [[ "$URL" == *".mp3"* ||  "$URL" == *".flac"* || "$URL" == *".m4a"* || "$URL" == *".wav"* || "$URL" == *".wma"* || "$URL" == *".aac"* ]]; then
-        # Audio, mainly for podcasts
+    else    # Non-200 HTML code
+        notify-send "Error $HTML_CODE" "Cannot open $URL"
+    fi
+elif [[ "$URL" == *".mp3"* ||  "$URL" == *".flac"* || "$URL" == *".m4a"* || "$URL" == *".wav"* || "$URL" == *".wma"* || "$URL" == *".aac"* ]]; then
+    # Audio, mainly for podcasts
+    HTML_CODE=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' "$URL")
+    if [[ $(echo "$HTML_CODE" | head -c 1) == "2" ]]; then
         eval "$PROG_AUDIO $URL & 2>/dev/null"
         Opened_app=$(echo "$PROG_AUDIO" | head -n1 | cut -d " " -f1)
-    else
-        eval "$BROWSER $URL &> /dev/null 2>&1 &"
-        Opened_app=$(echo "$BROWSER" | head -n1 | cut -d " " -f1)
+    else    # Non-200 HTML code
+        notify-send "Error $HTML_CODE" "Cannot open $URL"
     fi
-else    # Non-200 HTML code
-    notify-send "Error $HTML_CODE" "Cannot open $URL"
+else
+    eval "$BROWSER $URL &> /dev/null 2>&1 &"
+    Opened_app=$(echo "$BROWSER" | head -n1 | cut -d " " -f1)
 fi
+
 
 if [ $NOTIFY_ON_OPEN -eq 1 ]; then
     notify-send "Opened in $Opened_app: $URL"
