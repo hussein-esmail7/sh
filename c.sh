fe3a4e6dea53b632798693ec63ad0a2f88cb5073
Hussein Esmail <hussein.esmail7@gmail.com>
2021-08-22 09:32:13 -0400

    modified:   c.sh

--- c.sh
+++ c.sh
@@ -3,19 +3,22 @@
 # c.sh
 # Hussein Esmail
 # Created: 2021 01 09
-# Updated: 2021 05 14
+# Updated: 2021 08 22
 # Description: This script will compile or run other finishing operation on a
-# document.  
-# tex files: Compiles to pdf. Including bibliography if nessesary
-# c files: Compiles via whatever compiler is set to cc. Usually gcc.
-# py files: Runs via python command
-# All others: Open in default application
+# document. It determines what to do based on the document type
+# - tex files: Compiles to pdf. Including bibtex/makeglossaries if nessesary
+# - c files: Compiles via whatever compiler is set to cc. Usually gcc.
+# - py files: Runs via python command
+# - All others: Open in default application
 
-if [ -z $1 ] ; then # If no filename is given
+if [ -z $1 ] ; then         # If no filename is given
     echo "Filename required."
     exit 1
-elif [ ! -z $2 ] ; then # If too many arguments are given
+elif [ ! -z $2 ] ; then     # If too many arguments are given
     echo "Too many arguments! If your file has spaces, use quotation marks or '\'."
+    exit 1
+elif [ ! -f "$1" ] ; then
+    echo "File does not exist in directory!"
     exit 1
 fi
 
@@ -24,33 +27,19 @@ dir=$(dirname "$file")  # Directory of the file
 base="${file##*.}"      # File extension only
 shebang="${file%.*}"    # File name before the extension
 
-cd "$dir" || exit 1 # If the terminal is not in an existing directory, exit the program
-
 case "$file" in
-    *\.c) gcc -o "$shebang" "$file" ; chmod +x "$shebang" ; "$dir"/"$shebang" ;;
-    *\.cpp) g++ -o "$shebang" "$file" ; chmod +x "$shebang" ; "$dir"/"$shebang" ;;
+    *\.c) gcc -o "$shebang" "$file" ; chmod +x "$shebang" ; ./"$shebang" ;;
+    *\.cpp) g++ -o "$shebang" "$file" ; chmod +x "$shebang" ; ./"$shebang" ;;
     *\.html) open "$file" ;;
     *\.java) java "$file" ;;
     *\.py) python3 "$file" ;;
     *\.sh) chmod +x "$file" && ./"$file" ;; 
     *\.tex) 
-        output=$(pdflatex -halt-on-error "$file" | grep '^!.*' -A200 --color=always)
-        # grep -c -e "fi" -e "if" c.sh
-        [[ grep -c -e "\cite{" -e "\bibitem{" -e "\bibliorgraphy{" -e "\bibliographystyle{" "$file" ]]; bibtex "$file"
-        output=$(pdflatex -halt-on-error "$file" | grep '^!.*' -A200 --color=always)
-        for i in {1..2}; do # Sometimes it needs to be compiled twice
-            output=$(pdflatex -halt-on-error "$file" | grep '^!.*' -A200 --color=always)
-            if [ -n "$output" ] ; then # If output is not blank
-                echo "$output"
-                exit 1
-            fi
-        done
-        # Remove all the junk/log files that were also created.
-        # If there is an error in the .tex document then the program would end before this happens
-        rm -f "__latexindent_temp.tex" "${shebang}.fdb_latexmk" "${shebang}.fls" \
-			"${shebang}.log" "${shebang}.aux" "${shebang}.synctex.gz" \
-			"${shebang}.out" "${shebang}.toc"
-		
+        pdflatex --interaction=batchmode "$file"
+        [[ $(grep "\cite{" "$file" | wc -l) -ge 1 || $(grep "\bibitem{" "$file" | wc -l) -ge 1 || $(grep "\bibliorgraphy{" "$file" | wc -l) -ge 1 || $(grep "\bibliographystyle{" "$file" | wc -l) -ge 1 ]]; bibtex "$shebang"
+        [[ $(grep "\newglossaryentry" "$file" | wc -l) -ge 1 ]]; makeglossaries "$shebang"
+        pdflatex --interaction=batchmode "$file"
+        rm -f "__latexindent_temp.tex" "${shebang}.fdb_latexmk" "${shebang}.fls" "${shebang}.log" "${shebang}.aux" "${shebang}.synctex.gz" "${shebang}.out" "${shebang}.toc" "${shebang}.run.xml" "${shebang}.bbl" "${shebang}.blg"
     ;;
     *) open "$file" ;; # Default statement
 esac
