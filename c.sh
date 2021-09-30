9f667fac8d10e12747afa56f053d6423a4774e10
Hussein Esmail <hussein.esmail7@gmail.com>
2021-09-29 21:04:26 -0400

    modified file

--- c.sh
+++ c.sh
@@ -3,27 +3,39 @@
 # c.sh
 # Hussein Esmail
 # Created: 2021 01 09
-# Updated: 2021 08 22
+# Updated: 2021 09 29
 # Description: This script will compile or run other finishing operation on a
 # document. It determines what to do based on the document type
-# - tex files: Compiles to pdf. Including bibtex/makeglossaries if nessesary
-# - c files: Compiles via whatever compiler is set to cc. Usually gcc.
-# - py files: Runs via python command
+# - .tex files: Compiles to pdf. Including bibtex/makeglossaries if nessesary
+# - .c files: Compiles via whatever compiler is set to cc. Usually gcc.
+# - .py files: Runs via python command
 # - All others: Open in default application
 
-if [ -z $1 ] ; then         # If no filename is given
-    echo "Filename required."
-    exit 1
-elif [ ! -z $2 ] ; then     # If too many arguments are given
-    echo "Too many arguments! If your file has spaces, use quotation marks or '\'."
-    exit 1
-elif [ ! -f "$1" ] ; then
-    echo "File does not exist in directory!"
+if [ -z "$1" ] ; then         # If no arguments are given
+    echo "usage: c <files>"
+    echo ""
+    echo "This program compiles given file names."
+    echo ""
+    echo ".tex: Converts to PDF (with BibTeX, makeglossaries) if necessary."
+    echo ".c: Compiles using gcc"
+    echo ".cpp: Compiles using g++"
+    echo ".html: Uses 'open' command"
+    echo ".java: Runs using 'java' command"
+    echo ".py: Runs using python3"
+    echo ".sh: Runs in terminal"
+    echo "All other file extensions: uses 'open' command"
+    echo ""
+    echo "See https://github.com/hussein-esmail7/sh for more info."
     exit 1
 fi
 
 for file in "$@" # file = given argument. You can pass multiple files at the same time.
 do
+    
+    elif [ ! -f "$1" ] ; then   # If file is not in the current dir or does not exist if given the full path
+        echo "File does not exist in directory!"
+        exit 1
+    fi
     dir=$(dirname "$file")  # Directory of the file
     base="${file##*.}"      # File extension only
     shebang="${file%.*}"    # File name before the extension
