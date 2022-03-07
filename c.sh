9f43931e319f372b0902950ddfd1ff3ea1a562a6
Hussein Esmail <hussein.esmail7@gmail.com>
2022-03-06 21:24:36 -0500

    Added bibtex file removals upon completion

--- c.sh
+++ c.sh
@@ -67,10 +67,12 @@ do
             [[ $(grep "\cite{" "$file" | wc -l) -ge 1 || $(grep "\bibitem{" "$file" | wc -l) -ge 1 || $(grep "\bibliorgraphy{" "$file" | wc -l) -ge 1 || $(grep "\bibliographystyle{" "$file" | wc -l) -ge 1 ]]; bibtex "$shebang"
             [[ $(grep "\newglossaryentry" "$file" | wc -l) -ge 1 ]]; makeglossaries "$shebang"
             $LATEX_ENGINE --interaction=batchmode "$file"
-            rm -f "__latexindent_temp.tex" "${shebang}.fdb_latexmk" "${shebang}.fls" "${shebang}.log" "${shebang}.aux" "${shebang}.synctex.gz" "${shebang}.out" "${shebang}.toc" "${shebang}.run.xml" "${shebang}.bbl" "${shebang}.blg" "${shebang}.bcf" "${shebang}.mx1"
+            rm -f "__latexindent_temp.tex" "${shebang}.fdb_latexmk" "${shebang}.fls" "${shebang}.log" "${shebang}.aux" "${shebang}.synctex.gz" "${shebang}.out" "${shebang}.toc" "${shebang}.run.xml" "${shebang}.bbl" "${shebang}.blg" "${shebang}.bcf" "${shebang}.mx1" "${shebang}.nav" "${shebang}.snm"
 			# .toc: Used to make table of contents
 			# .bbl: Used for bibliographies
 			# .mx1: Used for musictex library (typing sheetmusic)
+			# .nav: beamer temporary table of contents file
+			# .snm: Used with beamer, don't know what this is for. Empty file
         ;;
         *) open "$file" ;; # Default statement
     esac
