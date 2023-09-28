a133db7ea95e063510602bad0385334f690adf3f
Hussein Esmail <hussein.esmail7@gmail.com>
2023-09-28 14:04:16 -0400

    Moved all questions to beginning of program

--- update.sh
+++ update.sh
@@ -4,67 +4,105 @@
 # PROGRAM DESCRIPTION
 # This program performes all various update commands so I
 # dont have to worry about updating specific apps
-
-# Reload newsboat articles
-newsboat -x reload
-echo "Newsboat updated."
+#
+YN_PACMAN=0
+YN_NEWSBOAT=0
+YN_BREW=0
+YN_MSOFFICE=0
+YN_PIP=0
+YN_GIT=0
 
 os=$(uname -a | awk '{print $(1);}') # This command works on macOS and Linux
 
+# Ask user which things they want to update at the very beginning
 if [[ "$os" == "Linux" ]]; then # Linux System
-    sudo pacman -S archlinux-keyring && sudo pacman -Syyu
-    yay -Syyu --devel   # Update AUR packages + git dependencies
+	if command -v newsboat &> /dev/null ; then # If newsboat is installed
+	    while true; do
+	        read -p "Update newsboat [y/n]? " yn
+	        case $yn in
+	            [Yy]* ) YN_NEWSBOAT=1 ; break;;
+	            [Nn]* ) break ;;
+	            * ) echo "Please answer yes or no.";;
+	        esac
+	    done
+	fi
+	if command -v pacman &> /dev/null ; then # Linux System
+	    while true; do
+	        read -p "Update pacman packages [y/n]? " yn
+	        case $yn in
+	            [Yy]* ) YN_PACMAN=1 ; break;;
+	            [Nn]* ) break ;;
+	            * ) echo "Please answer yes or no.";;
+	        esac
+	    done
+	fi
 elif [[ "$os" == "Darwin" ]]; then # macOS System
-    # Update homebrew packages and apps
-    while true; do
-        read -p "Update brew packages [y/n]? " yn
-        case $yn in
-            [Yy]* ) brew update > /dev/null; brew upgrade > /dev/null; break;;
-            [Nn]* ) exit;;
-            * ) echo "Please answer yes or no.";;
-        esac
-    done
-    # Update Microsoft Office Applications
+	if command -v newsboat &> /dev/null ; then # If newsboat is installed
+	    while true; do
+	        read -p "Update newsboat [y/n]? " yn
+	        case $yn in
+	            [Yy]* ) YN_NEWSBOAT=1 ; break;;
+	            [Nn]* ) break ;;
+	            * ) echo "Please answer yes or no.";;
+	        esac
+	    done
+	fi
+	if command -v brew &> /dev/null ; then # If brew/homebrew is installed
+		while true; do
+    	    read -p "Update brew packages [y/n]? " yn
+    	    case $yn in
+    	        [Yy]* ) YN_BREW=1 ; break;;
+    	        [Nn]* ) break ;;
+    	        * ) echo "Please answer yes or no.";;
+    	    esac
+    	done
+	fi
     while true; do
         read -p "Update Microsoft applications [y/n]? " yn
         case $yn in
-            [Yy]* ) /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app/Contents/MacOS/msupdate --install --apps MSWD2019 XCEL2019 PPT32019 > /dev/null; break;;
-            [Nn]* ) exit;;
+            [Yy]* ) YN_MSOFFICE=1 ; break;;
+            [Nn]* ) break ;;
             * ) echo "Please answer yes or no.";;
         esac
     done
 fi
+if command -v python3 &> /dev/null ; then # If python3/pip is installed
+	while true; do
+	    read -p "Update pip packages [y/n]? " yn
+	    case $yn in
+	        [Yy]* ) YN_PIP=1 ; break;;
+	        [Nn]* ) break ;;
+	        * ) echo "Please answer yes or no.";;
+	    esac
+	done
+fi
 
-while true; do
-    read -p "Update pip packages [y/n]? " yn
-    case $yn in
-        [Yy]* )
-            # Update pip
-            pip3 --quiet --no-input install --upgrade pip
-            python3 -m pip install --upgrade pip
-            /usr/bin/python -m pip install --upgrade pip
-            # Update all pip packages
-            pip freeze --user | cut -d'=' -f1 | xargs -n1 pip install -U
-            python3 -m pip freeze --user | cut -d'=' -f1 | xargs -n1 python3 -m pip install -U
-            break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+# Reload newsboat articles
+if $YN_NEWSBOAT -eq 1 ; then
+	# If 'update newsboat' is 'yes' and newsboat is installed
+	newsboat -x reload
+	echo "Newsboat updated."
+fi
+if $YN_BREW -eq 1 ; then
+	# If 'update brew packages' is 'yes' and brew is installed
+	brew update > /dev/null; brew upgrade > /dev/null
+fi
 
-while true; do
-    read -p "Update git clones [y/n]? " yn
-    case $yn in
-        [Yy]* )
-            IFS=$'\n' git_locations=($(find $HOME -name ".git"))
-            for i in "${git_locations[@]}" ; do
-                cd "${i%.*}" ; echo "Updating git folder $i" ; git pull || echo "Could not update $i"
-            done
-            cd
-            break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+if $YN_PACMAN -eq 1 ; then # Linux System
+    sudo pacman -S archlinux-keyring && sudo pacman -Syyu
+    yay -Syyu --devel   # Update AUR packages + git dependencies
+fi
+if $YN_MSOFFICE -eq 1 ; then # Update MS Office on Mac
+    /Library/Application\ Support/Microsoft/MAU2.0/Microsoft\ AutoUpdate.app/Contents/MacOS/msupdate --install --apps MSWD2019 XCEL2019 PPT32019 > /dev/null; break;;
+fi
+
+if $YN_PIP -eq 1 ; then # Update MS Office on Mac
+    pip3 --quiet --no-input install --upgrade pip
+    python3 -m pip install --upgrade pip
+    /usr/bin/python -m pip install --upgrade pip
+    # Update all pip packages
+    pip freeze --user | cut -d'=' -f1 | xargs -n1 pip install -U
+    python3 -m pip freeze --user | cut -d'=' -f1 | xargs -n1 python3 -m pip install -U
+fi
 
 exit 0
