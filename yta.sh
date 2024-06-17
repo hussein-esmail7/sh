bff2ee01c5ba0aa33cec27eb0e018c3e7fb7f3e3
Hussein Esmail <hussein.esmail7@gmail.com>
2024-06-17 15:36:17 -0400

    Multiple URLs

--- yta.sh
+++ yta.sh
@@ -2,18 +2,13 @@
 
 # Program description:
 # This program takes a YouTube URL and runs yt-dlp to get the audio only
-string="$1"
-if [ -z $1 ] ; then
-	# If no argument given
-    echo -n "URL: "
-    read string
-    if [ "$string" == "exit" ] || [ "$string" == "q" ] || [ "$string" == "q" ] ; then
-        exit 1
-    fi
+string="$@"
+if [ -z "$string" ] ; then
+	# If string is empty, exit
+	echo "No input given."
+    exit 1
 fi
 
-string=${string#"https://www.youtube.com/watch?v="} || echo -n ""
-# echo "$string"
 yt-dlp -ic -R 100 -x --no-check-certificate \
 --yes-playlist --skip-unavailable-fragments --restrict-filenames \
 --no-warnings --no-part -f best --audio-format mp3 -o \
